c57e6dd90c45bff5a2e8e546537d431fb678ab02
BlakeBrisben <brisbenblake@gmail.com>
2024-10-30 21:57:35 -0600

    Some early stuff

---
+++ kilo.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <termios.h>
+
+void enableRawMode() {
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+// At 02 Turning off Echoing
+int main() {
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    return 0;
+}
